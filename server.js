c3d4c06f5ffb32e10155030334d0e95c34713600
Md Saiyad Husain <MDSaiyad.husain@cognizant.com>
2019-05-19 14:39:38 +0530

    header added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const UserModel = require("./model/model");
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    next(); 
 })
 app.listen(3000, () => {
     console.log("Listening at :3000...");
